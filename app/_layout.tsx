17d8c28a9868bd91c063e396b454dbf958055005
githubfacu <elorzfacundo@gmail.com>
2025-12-17 22:20:06 -0300

    feat: Theme changer

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,4 +1,3 @@
-import { DarkTheme, DefaultTheme, ThemeProvider } from '@react-navigation/native';
 import { StatusBar } from 'expo-status-bar';
 import { GestureHandlerRootView } from 'react-native-gesture-handler';
 
@@ -9,6 +8,7 @@ import { useThemeColor } from '@/hooks/use-theme-color';
 import { Stack } from 'expo-router';
 
 import { allRoutes } from '@/constants/Routes';
+import { ThemeChangerProvider } from '@/presentation/context/ThemeChangerContext';
 import "../global.css";
 
 export const unstable_settings = {
@@ -23,7 +23,7 @@ export default function RootLayout() {
     <GestureHandlerRootView
       style={{ backgroundColor: backgroundColor, flex: 1}}
     >
-      <ThemeProvider value={colorScheme === 'dark' ? DarkTheme : DefaultTheme}>
+      <ThemeChangerProvider>
         <Stack
           screenOptions={{
             headerShadowVisible: false,
@@ -55,7 +55,7 @@ export default function RootLayout() {
           }
         </Stack>
         <StatusBar style="auto" />
-      </ThemeProvider>      
+      </ThemeChangerProvider>      
     </GestureHandlerRootView>
 
   );
--- app/themes/index.tsx
+++ app/themes/index.tsx
@@ -1,10 +1,53 @@
-import { View, Text } from 'react-native';
+import { useThemeChangerContext } from '@/presentation/context/ThemeChangerContext';
+import ThemedCard from '@/presentation/shared/ThemedCard';
+import ThemedSwitch from '@/presentation/shared/ThemedSwitch';
+import ThemedView from '@/presentation/shared/ThemedView';
+import { useState } from 'react';
 
 const ThemesScreen = () => {
+
+  const { toggleTheme, currenTheme, setSystemTheme, isSystemTheme } = useThemeChangerContext();
+
+  const [darkModeSettings, setDarkModeSettings] = useState({
+    darkMode: currenTheme === 'dark',
+    systemMode: isSystemTheme,
+  });
+
+  const setDarkMode = (value: boolean) => {
+    toggleTheme()
+    setDarkModeSettings({
+      darkMode: value,
+      systemMode: false,
+    });
+  };
+
+  const setSystemMode = (value: boolean) => {
+    if (value) {
+      setSystemTheme();
+    }
+    setDarkModeSettings({
+      darkMode: darkModeSettings.darkMode,
+      systemMode: value,
+    });
+  };
+
   return (
-    <View>
-      <Text>ThemesScreen</Text>
-    </View>
+    <ThemedView margin>
+      <ThemedCard className='mt-5'>
+        <ThemedSwitch
+          text='Modo oscuro'
+          className='mb-5'
+          value={ darkModeSettings.darkMode }
+          onValueChange={ setDarkMode }
+        />
+
+        <ThemedSwitch
+          text='Modo sistema'
+          value={ darkModeSettings.systemMode }
+          onValueChange={ setSystemMode }
+        />
+      </ThemedCard>
+    </ThemedView>
   );
 };
 export default ThemesScreen;
